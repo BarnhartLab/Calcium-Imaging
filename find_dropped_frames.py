d5efc83b6259a19ab164609019407fc6bb15daac
BarnhartLab <105936292+BarnhartLab@users.noreply.github.com>
2022-05-23 15:37:09 -0400

    Update find_dropped_frames.py

--- find_dropped_frames.py
+++ find_dropped_frames.py
@@ -1,16 +1,16 @@
 import numpy
-import ResponseClassSimple_EB
-import ResponseTools_V4
+import ResponseClassSimple
+import ResponseTools
 import os
 from pylab import *
 
-parent_dir = '/Users/katherinedelgado/Desktop/Lab_2022/Mi1_Activity/'
+parent_dir = '//'
 
 input_csv = parent_dir+'inputs.csv'
-rows,header = ResponseTools_V4.read_csv_file(input_csv)
+rows,header = ResponseTools.read_csv_file(input_csv)
 
 for row in rows:
-	input_dict = ResponseTools_V4.get_input_dict(row,header)
+	input_dict = ResponseTools.get_input_dict(row,header)
 	#print(input_dict)
 	print('checking stim file for sample ' + input_dict['sample_name'] + ' ' + input_dict['reporter_name'] + ' ' + input_dict['stimulus_name'])
 	sample_dir = parent_dir+input_dict['sample_name']
@@ -20,20 +20,20 @@ for row in rows:
 	if not os.path.exists(output_dir):
 		os.makedirs(output_dir)
 	
-	stim_file = ResponseTools_V4.get_file_names(stim_dir,file_type = 'csv',label = input_dict['stimulus_name'])[0]
+	stim_file = ResponseTools.get_file_names(stim_dir,file_type = 'csv',label = input_dict['stimulus_name'])[0]
 	if input_dict['aligned']=='TRUE':
 		image_file = image_dir + input_dict['ch1_name']+'-aligned.tif'
 	else:
 		image_file = image_dir + input_dict['ch1_name']+'.tif'
 
-	I = ResponseTools_V4.read_tifs(image_file)
+	I = ResponseTools.read_tifs(image_file)
 
 	frames = len(I)
 	time_interval = float(input_dict['time_interval'])
 	gt_index = int(input_dict['gt_index'])
 
 	stim_data,stim_data_OG,dataheader = ResponseTools_V4.count_frames(stim_file,gt_index = gt_index)
-	ResponseTools_V4.find_dropped_frames(frames,time_interval,stim_data,stim_data_OG,gt_index)
+	ResponseTools.find_dropped_frames(frames,time_interval,stim_data,stim_data_OG,gt_index)
 
 	if input_dict['verbose']=='TRUE':
-		ResponseTools_V4.write_csv(stim_data,dataheader,stim_dir + 'parsed/parsed-'+input_dict['stimulus_name'] +'.csv')
+		ResponseTools.write_csv(stim_data,dataheader,stim_dir + 'parsed/parsed-'+input_dict['stimulus_name'] +'.csv')
